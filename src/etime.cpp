e120829471a840807d604a2a24e3211049e6b807
Samuel Déal <samuel.deal@gmail.com>
2014-08-09 05:22:37 +0200

    add some debug info

--- src/etime.cpp
+++ src/etime.cpp
@@ -1,6 +1,7 @@
 #include "etime.h"
 
 #include <cstddef>
+#include <iostream>
 
 const ETime& ETime::oneSecond() {
     static ETime constant(1, 0);
@@ -11,29 +12,30 @@ const ETime& ETime::oneSecond() {
 
 ETime::ETime() {
     gettimeofday(&_data, NULL);
-    std::cout<< "now = " << _data.tv_sec << "," << _data.tv_usec;
+    std::cout<< "created now: " << _data.tv_sec << "," << _data.tv_usec << std::endl;
 }
 
 ETime::ETime(long sec, long usec){
     _data.tv_sec = sec;
     _data.tv_usec = usec;
-    std::cout<< "created = " << _data.tv_sec << "," << _data.tv_usec;
+    std::cout<< "created fixed: " << _data.tv_sec << "," << _data.tv_usec << std::endl;
 }
 
 ETime ETime::operator-(const ETime &otherTime) const {
     long long int diff = (_data.tv_usec + 1000000  * _data.tv_sec) - (otherTime._data.tv_usec + 1000000 * otherTime._data.tv_sec);
+    std::cout<< "creating minus" << std::endl;
     return ETime(diff / 1000000, diff % 1000000);
 }
 
 bool ETime::operator>(const ETime &otherTime) const{
     if(_data.tv_sec > otherTime._data.tv_sec){
-        std::cout<< "comp " << _data.tv_sec << ">" << otherTime._data.tv_sec << "=> true";
+        std::cout<< "comp " << _data.tv_sec << ">" << otherTime._data.tv_sec << "=> true"  << std::endl;
         return true;
     }
     if(_data.tv_sec < otherTime._data.tv_sec){
         return false;
-        std::cout<< "comp " << _data.tv_sec << "<" << otherTime._data.tv_sec << "=> false";
+        std::cout<< "comp " << _data.tv_sec << "<" << otherTime._data.tv_sec << "=> false"  << std::endl;
     }
-    std::cout<< "comp " << _data.tv_usec << ">" << otherTime._data.tv_usec << "=> " << (_data.tv_usec > otherTime._data.tv_usec) ? "true" : "false";
+    std::cout<< "comp " << _data.tv_usec << ">" << otherTime._data.tv_usec << "=> " << (_data.tv_usec > otherTime._data.tv_usec) ? "true" : "false" << std::endl;
     return _data.tv_usec > otherTime._data.tv_usec;
 }
