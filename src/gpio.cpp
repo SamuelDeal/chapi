4e378a0af9ba16dc92bd8743387a688cd6266c96
sam <samuel.deal@gmail.com>
2015-02-28 17:00:14 +0100

    temp

--- src/gpio.cpp
+++ src/gpio.cpp
@@ -1,35 +1,76 @@
 #include "gpio.h"
 
-#ifndef NOGPIO
-#include <wiringPi.h>
-#endif
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
 
-Gpio::In::In(unsigned char pin) {
+
+
+static const char *export_filepath = "/sys/class/gpio/export";
+static void export_gpio(unsigned char pin) {
+    std::ofstream sysfile(export_filepath);
+    if (!sysfile.is_open()){
+        throw "TODO: OPERATION FAILED: Unable to export GPIO ";
+    }
+    sysfile << pin;
+}
+
+static const char *unexport_filepath = "/sys/class/gpio/unexport";
+static void Gpio::Core::unexport_gpio(unsigned char pin) {
+    std::ofstream sysfile(unexport_filepath);
+    if (!sysfile.is_open()){
+        throw "TODO: OPERATION FAILED: Unable to export GPIO ";
+    }
+    sysfile << pin;
+}
+
+void Gpio::Core::set_gpio_dir(unsigned char pin, Direction dir) {
+    std::string filepath = "/sys/class/gpio/gpio" + std::to_string(pin); + "/direction";
+    std::ofstream sysfile(filepath.c_str());
+    if (!sysfile.is_open()){
+        throw "TODO: OPERATION FAILED: Unable to set direction of GPIO ";
+    }
+    sysfile << (dir == Gpio::Core::In) ? "in" : "out";
+}
+
+
+Gpio::Core::Core(unsigned char pin, Direction dir) {
     _pin = pin;
-#ifndef NOGPIO
-    wiringPiSetupPhys();
-    pinMode((int) pin, INPUT);
-#endif
+    export_gpio(_pin);
+    set_gpio_dir(_pin, dir);
+}
+
+Gpio::Core::~Core() throw {
+    try{
+        unexport_gpio(_pin);
+    }
+    catch(...){
+    }
+}
+
+Gpio::In::In(unsigned char pin) : Gpio::Core(pin, Direction::In){
+    _filepath = "/sys/class/gpio/gpio" + std::to_string(_pin); + "/value";
 }
 
 Gpio::Value Gpio::In::read() {
-#ifdef NOGPIO
-    return Gpio::Low;
-#else
-    return ((digitalRead((int)_pin) == HIGH) ? Gpio::High : Gpio::Low);
-#endif
+    std::ifstream sysfile(_filepath.c_str());
+    if (!sysfile.is_open()){
+        throw "TODO: OPERATION FAILED: Unable to set direction of GPIO ";
+    }
+    std::string value;
+    getvalgpio >> value;
+    return (value == "0") ? Gpio::Low : Gpio::High;
 }
 
-Gpio::Out::Out(unsigned char pin) {
-    _pin = pin;
-#ifndef NOGPIO
-    wiringPiSetupPhys();
-    pinMode((int) pin, OUTPUT);
-#endif
+Gpio::Out::Out(unsigned char pin) : Gpio::Core(pin, Direction::Out) {
+    _filepath = "/sys/class/gpio/gpio" + std::to_string(_pin); + "/value";
 }
 
 void Gpio::Out::write(Gpio::Value value) {
-#ifndef NOGPIO
-    digitalWrite((int)_pin, (value == Gpio::High)? HIGH: LOW);
-#endif
+    std::ofstream sysfile(_filepath.c_str());
+    if (!sysfile.is_open()){
+        throw "TODO: OPERATION FAILED: Unable to set direction of GPIO ";
+    }
+    sysfile << (value == Gpio::High) ? "1" : "0";
 }
--- src/gpio.h
+++ src/gpio.h
@@ -1,29 +1,47 @@
 #ifndef GPIO_H
 #define GPIO_H
 
+#include <string>
+
 namespace Gpio {
-    enum Value {
-        Low,
-        High
+
+enum Value {
+    Low = 0,
+    High = 1
+};
+
+class Core {
+protected:
+    enum Direction {
+        In,
+        Out
     };
 
-    class In {
-    public:
-        In(unsigned char pin);
-        Gpio::Value read();
+    unsigned char _pin;
 
-    private:
-        unsigned char _pin;
-    };
+    Core(unsigned char pin, Direction dir);
+    ~Core() throw;
+};
 
-    class Out {
-    public:
-        Out(unsigned char pin);
-        void write(Gpio::Value);
 
-    private:
-        unsigned char _pin;
-    };
-}
+class In: public Core {
+public:
+    In(unsigned char pin);
+    Gpio::Value read();
+
+protected:
+    std::string _filepath;
+};
+
+class Out: public Core {
+public:
+    Out(unsigned char pin);
+    void write(Gpio::Value);
+
+protected:
+    std::string _filepath;
+};
+
+
 
 #endif // GPIO_H
